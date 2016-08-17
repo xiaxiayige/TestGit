642680c5ce46bad9828966cd3325189c501203c1
zql <zhangql@civaonline.cn>
2016-08-17 17:02:13 +0800

    第一次提交

---
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.testgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/***
+ * 测试git 提交 冲突 合并
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
