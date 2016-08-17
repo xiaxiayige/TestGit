3690d1fa802e905b778d743b80ccdbc30c80a33c
zql <zhangql@civaonline.cn>
2016-08-17 17:14:15 +0800

    我添加了一行注释

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //本地修改MainActiviy 提交到服务器
     }
 }
